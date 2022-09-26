610a55647534b33ab53cefcca1d89aca829e25a2
Lezhok <112374444+Lezhok@users.noreply.github.com>
2022-09-26 15:17:34 +0300

    Add files via upload

---
+++ lab2_4/lab2_4/Program.cs
@@ -0,0 +1,34 @@
+﻿class User
+{
+    public string login = "olehvolosatskyi@gmail.com";
+    public string name = "Oleh";
+    public string surname = "Volosatskyi";
+    public int age = 18;
+    public readonly DateTime date = DateTime.Now;
+
+
+    public User(string loginx, string namex, string surnamex, int agex)
+    {
+        loginx = login;
+        namex = name;
+        surnamex = surname;
+        agex = age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User solution\n");
+
+            User ezUser = new User("olehvolosatskyi@gmail.com", "Oleh", "Volosatskyi", 18);
+
+            Console.WriteLine(ezUser.login);
+            Console.WriteLine(ezUser.name);
+            Console.WriteLine(ezUser.surname);
+            Console.WriteLine(ezUser.age);
+            Console.WriteLine(ezUser.date);
+
+            Console.ReadKey();
+        }
+    }
+}
